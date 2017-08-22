dc877996f7b0a4306c38287770148d3310e06d1f
georgerappel <george.concei@hotmail.com>
2017-08-22 17:03:55 -0300

    Fix columns before migrating

--- db/migrate/20170821082243_create_curso.rb
+++ db/migrate/20170821082243_create_curso.rb
@@ -2,7 +2,9 @@ class CreateCurso < ActiveRecord::Migration[5.1]
   def change
     create_table :cursos do |t|
       t.string :name
-      t.integer :departamento
+      t.integer :departamento_id
+
+      t.timestamps
     end
   end
 end
