39dd94c9338771d878f5ac64d40c8e9b18a7d15e
georgerappel <george.concei@hotmail.com>
2017-08-23 19:12:19 -0300

    Test primary key

--- db/migrate/20170625220313_create_disciplinas.rb
+++ db/migrate/20170625220313_create_disciplinas.rb
@@ -1,10 +1,11 @@
 class CreateDisciplinas < ActiveRecord::Migration[5.1]
   def change
-    create_table :disciplinas do |t|
-      t.string :codigo, index: { unique: true }
+    create_table :disciplinas, id: false do |t|
+      t.string :codigo, null: false
       t.string :nome
 
       t.timestamps
     end
+    add_index :disciplinas, [:codigo], unique: true
   end
 end
